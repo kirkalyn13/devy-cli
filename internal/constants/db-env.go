13875c470097fa089d69546b9f6df439807329ee
Kirk Alyn Santos <kirkalyn13@gmail.com>
2022-10-27 20:47:25 +0800

    added constant db env

---
+++ internal/constants/db-env.go
@@ -0,0 +1,9 @@
+// Using default generic values. Please use secrets or env files!
+package constants
+
+const (
+	DB_HOST     = "localhost"
+	DB_PORT     = "3306"
+	DB_USERNAME = "root"
+	DB_PASSWORD = "root"
+)
